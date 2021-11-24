1ae8f13d5649c7b4be92bc22ebf49d948c7860f4
59132412 <dieptuthinh@gmail.com>
2021-11-24 21:59:03 +0700

    i18n

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -90,9 +90,7 @@ const Header = () => {
                         }
                     </div>
                     <div className="header__menu__right">
-                        {/* <div className="header__menu__item header__menu__right__item">
-                            <i className="bx bx-search"></i>
-                        </div> */}
+
                         <div className="header__menu__item header__menu__right__item">
                             <Link to="/cart" className="header__menu__right__item-cart">
                                 <i className="bx bx-shopping-bag"></i>
@@ -129,7 +127,9 @@ const Header = () => {
                         }
 
 
-
+                        <div className="header__menu__item header__menu__right__item">
+                            <i className="bx bx-world"></i>
+                        </div>
                     </div>
                 </div>
             </div>
